a00d144ff993257b4e618ee43bd0076706307e18
natanaelvich <taelima1997@gmail.com>
2020-03-24 18:29:57 -0300

    added cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const server = express();
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
