b329da27a6947dbaba96adcd4b33ef37f1e50cc3
natanaelvich <taelima1997@gmail.com>
2020-03-29 14:03:55 -0300

    added validation inputs

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import OngController from './controllers/OngController';
 import IncidentsController from './controllers/IncidentsController';
 import ProfileController from './controllers/ProfileController';
@@ -8,13 +10,57 @@ const routes = Router();
 
 routes.post('/session', SessionController.store);
 
-routes.post('/ongs', OngController.store);
 routes.get('/ongs', OngController.index);
+routes.post(
+  '/ongs',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number()
+        .required()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2),
+    }),
+  }),
+  OngController.store
+);
 
-routes.get('/ongs/incidents', IncidentsController.index);
+routes.get(
+  '/ongs/incidents',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+    }),
+  }),
+  IncidentsController.index
+);
+
 routes.post('/ongs/incidents', IncidentsController.store);
-routes.delete('/ongs/incidents/:id', IncidentsController.destroy);
+routes.delete(
+  '/ongs/incidents/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  IncidentsController.destroy
+);
 
-routes.get('/ongs/profile/incidents', ProfileController.index);
+routes.get(
+  '/ongs/profile/incidents',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      ong_id: Joi.string().required(),
+    }).unknown(),
+  }),
+  ProfileController.index
+);
 
 export default routes;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(routes);
+server.use(errors());
 
 server.listen(3333, () => {
   console.log('port 3333');
